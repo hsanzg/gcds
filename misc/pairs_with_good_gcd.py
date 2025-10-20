d804410a30f739bbd6b7a684ba703050888a3bcb
Hugo Sanz González <hsanzgon@bsc.es>
2025-10-20 20:52:38 +0200

    Add R. P. Brent's variant of binary gcd method

--- misc/pairs_with_good_gcd.py
+++ misc/pairs_with_good_gcd.py
@@ -1,22 +1,44 @@
 import math
+import random
+random.seed(2025)
 
 u_start, u_end, v_start, v_end = 50000, 70000, 18000, 30000
 u_start, u_end, v_start, v_end = 5000, 5100, 8000, 9000
 u_start, u_end, v_start, v_end = 28, 40, 65, 75
+u_start, u_end, v_start, v_end = 300, 310, 5000, 5100
+u_start, u_end, v_start, v_end = 7600, 7700, 3400, 3490
+u_start, u_end, v_start, v_end = 30000, 35000, 18000, 30000
 #u_start, u_end, v_start, v_end = 110, 130, 550, 555
-good_threshold = .002 # relative to `min(u,v)`.
+
+u_digits, v_digits = 4, 4
+u_start, v_start = 10 ** u_digits, 10 ** v_digits
+u_end, v_end = u_start * 10, v_start * 10
+
+good_threshold = .001 # relative to `min(u,v)`.
 too_good_threshold = .08 # exclusive.
-good_threshold, too_good_threshold = .1, .499
+#good_threshold, too_good_threshold = .5, .99
 limit = 100 # how many to print.
+mode = 'random' # or 'exhaustive'.
 
 print('u\tv\tgcd\tgoodness')
 found = 0
-for u in range(u_start, u_end):
-	for v in range(v_start, v_end):
-		gcd = math.gcd(u, v)
-		w = min(u, v)
-		if good_threshold * w <= gcd < too_good_threshold * w:
-			print(f'{u}\t{v}\t{gcd}\t{gcd / w}')
-			found += 1
+
+def process(u, v):
+	global found
+	gcd = math.gcd(u, v)
+	w = min(u, v)
+	if good_threshold * w <= gcd < too_good_threshold * w:
+		print(f'{u}\t{v}\t{gcd}\t{gcd / w}')
+		found += 1
+
+if mode == 'exhaustive':
+	for u in range(u_start, u_end):
+		for v in range(v_start, v_end):
+			process(u, v)
 			if found > limit:
 				break
+elif mode == 'random':
+	while found < limit:
+		u = random.randrange(u_start, u_end)
+		v = random.randrange(v_start, v_end)
+		process(u, v)
--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,8 @@
 //! The _greatest common divisor_ $\gcd(u,v)$ of two integers $u$ and $v$,
 //! not both zero, is the largest integer that evenly divides them both.
 //! (By convention we let $\gcd(0,0)=0$.)
-//#![no_std]
+//! All the algorithms adopt the convention $\gcd(0,0)=0$.
+#![cfg_attr(not(test), no_std)]
 
 /// Computes the greatest common divisor of $u$ and $v$ using the modern
 /// version of the Euclidean algorithm, as described in Algorithm 4.5.2A
@@ -24,6 +25,7 @@
 /// assert_eq!(euclid(7000, 4400), 200);
 /// assert_eq!(euclid(40902, 24140), 34);
 /// assert_eq!(euclid(u64::MAX, u64::MAX - 1), 1);
+/// assert_eq!(euclid(u64::MAX, u64::MAX), u64::MAX);
 ///
 /// // Find the gcd of two signed integers.
 /// assert_eq!(euclid((-9i64).unsigned_abs(), 3), 3);
@@ -41,36 +43,37 @@ pub const fn euclid(mut u: u64, mut v: u64) -> u64 {
   u
 }
 
-#[must_use]
-pub const fn binary_stein(mut u: u64, mut v: u64) -> u64 {
-  todo!()
-}
-
 /// Computes the greatest common divisor of $u$ and $v$ using the binary
-/// gcd algorithm of R. P. Brent [[_"Further analysis of the Binary Euclidean algorithm,"_][brent]
-/// Technical Report PRG TR-7-99 (Oxford Univ. Computing Laboratory, 1999)].
+/// gcd algorithm of J. Stein [["Computational Problems Associated with Racah Algebra,"][stein]
+/// _J. Comp. Phys._ **1** (1967), 397--405].
+///
+/// A summary of what was known up to 1999 about the average running time
+/// of this method appears in R. P. Brent's [_"Further analysis of the Binary Euclidean algorithm,"_][brent]
+/// Technical Report PRG TR-7--99 (Oxford Univ. Computing Laboratory, 1999).
+/// See also Section 4.5.2 of _TAOCP_.
 ///
 /// # Examples
 /// ```
-/// use gcd::binary_brent;
+/// use gcd::binary_stein;
 ///
-/// assert_eq!(binary_brent(0, 0), 0);
-/// assert_eq!(binary_brent(0, 3), 3);
-/// assert_eq!(binary_brent(1, 1), 1);
-/// assert_eq!(binary_brent(5, 1), 1);
-/// assert_eq!(binary_brent(114, 551), 19);
-/// assert_eq!(binary_brent(1992, 581), 83);
-/// assert_eq!(binary_brent(51041, 18017), 43);
-/// assert_eq!(binary_brent(u64::MAX, u64::MAX - 1), 1);
-/// assert_eq!(binary_brent(u64::MAX, u64::MAX), u64::MAX);
+/// assert_eq!(binary_stein(0, 0), 0);
+/// assert_eq!(binary_stein(0, 3), 3);
+/// assert_eq!(binary_stein(1, 1), 1);
+/// assert_eq!(binary_stein(5, 1), 1);
+/// assert_eq!(binary_stein(114, 551), 19);
+/// assert_eq!(binary_stein(1992, 581), 83);
+/// assert_eq!(binary_stein(51041, 18017), 43);
+/// assert_eq!(binary_stein(u64::MAX, u64::MAX - 1), 1);
+/// assert_eq!(binary_stein(u64::MAX, u64::MAX), u64::MAX);
 ///
 /// // Find the gcd of two signed numbers.
-/// assert_eq!(binary_brent(36, (-48i64).unsigned_abs()), 12);
+/// assert_eq!(binary_stein(36, (-48i64).unsigned_abs()), 12);
 /// ```
 ///
+/// [stein]: https://doi.org/10.1016/0021-9991(67)90047-2
 /// [brent]: https://maths-people.anu.edu.au/~brent/pd/rpb183tr.pdf
 #[must_use]
-pub fn binary_brent(mut u: u64, mut v: u64) -> u64 {
+pub fn binary_stein(mut u: u64, mut v: u64) -> u64 {
   if u == 0 {
     return v;
   }
@@ -113,6 +116,82 @@ pub fn binary_brent(mut u: u64, mut v: u64) -> u64 {
   }
 }
 
+/// Computes the greatest common divisor of $u$ and $v$ using an alternative
+/// formulation of the binary gcd algorithm proposed by R. P. Brent
+/// [[_"Further analysis of the Binary Euclidean algorithm,"_][brent]
+/// Technical Report PRG TR-7--99 (Oxford Univ. Computing Laboratory, 1999),
+/// Section 5].
+///
+/// # Examples
+/// ```
+/// use gcd::binary_brent;
+///
+/// assert_eq!(binary_brent(0, 0), 0);
+/// assert_eq!(binary_brent(0, 4), 4);
+/// assert_eq!(binary_brent(1, 1), 1);
+/// assert_eq!(binary_brent(1, 7), 1);
+/// assert_eq!(binary_brent(306, 5049), 153);
+/// assert_eq!(binary_brent(7684, 3400), 68);
+/// assert_eq!(binary_brent(68825, 10150), 25);
+/// assert_eq!(binary_brent(u64::MAX, u64::MAX - 1), 1);
+/// assert_eq!(binary_brent(u64::MAX, u64::MAX), u64::MAX);
+///
+/// // Find the gcd of two signed integers.
+/// assert_eq!(
+///   binary_brent((-10i64).unsigned_abs(), (-5i64).unsigned_abs()),
+///   5
+/// );
+/// ```
+///
+/// [brent]: https://maths-people.anu.edu.au/~brent/pd/rpb183tr.pdf
+pub fn binary_brent(mut u: u64, mut v: u64) -> u64 {
+  if u == 0 {
+    return v;
+  }
+  if v == 0 {
+    return u;
+  }
+  // A proof of correctness of this algorithm looks almost identical to
+  // that in `binary_stein`, the original version of the binary method.
+  // First recall that $\gcd(u,v)=2^k\gcd(u/2^{k_u},v/2^{k_v})$, where
+  // $2^{k_u}$ and $2^{k_v}$ are the greatest powers of 2 that divide
+  // $u$ and $v$, respectively, and $k=\min(k_u,k_v)$. (The integers
+  // $k_u$ and $k_v$ are sometimes known as the _dyadic valuations_
+  // of $u$ and $v$, respectively.)
+  let (k_u, k_v) = (u.trailing_zeros(), v.trailing_zeros());
+  // SAFETY: Since $u$ and $v$ are nonzero, $k_u$ and $k_v$ are
+  //         less than `u64::BITS`.
+  u >>= k_u;
+  v >>= k_v;
+  let k = k_u.min(k_v);
+  // Ensure that $u\le v$ on entry to R. P. Brent's Algorithm V.
+  // todo: check if moving this swap before the trailing zero computation
+  //  results in a measurable speedup.
+  if u > v {
+    (u, v) = (v, u);
+  }
+  while u != v {
+    while u < v {
+      // Note: This inner loop satisfies the invariant condition that
+      // $u$ and $v$ are both odd at the beginning of each iteration.
+      // Apply the identity $\gcd(u,v)=\gcd(u,v-u)$.
+      v -= u;
+      // Now $u$ is odd and $v$ is even; we can use the fact that
+      // $\gcd(u,v)=\gcd(u,v/2^{k_v})$, where $2^{k_v}$ divides $v$.
+      // SAFETY: $v$ is nonzero, because it was greater than $u$ before
+      //         the subtraction step.
+      v >>= v.trailing_zeros();
+      // At this point $v$ is odd, as required by the invariant.
+    }
+    (u, v) = (v, u);
+  }
+  // We have $\gcd(u,v)=u$; terminate with $u\cdot2^k$.
+  u << k
+}
+
+/// Computes the greatest common divisor of $u$ and $v$ using an algorithm
+/// of V. C. Harris, a cross between [Euclid's algorithm](euclid) and the [binary algorithm](binary_stein),
+/// as described in Section 4.5.2 of _TAOCP_.
 #[must_use]
 pub const fn harris(mut u: u64, mut v: u64) -> u64 {
   todo!()
@@ -120,6 +199,7 @@ pub const fn harris(mut u: u64, mut v: u64) -> u64 {
 
 #[must_use]
 pub fn binary_brent_kung(mut u: u64, mut v: u64) -> u64 {
+  // It can be shown that this loop runs for $\le 2+2\lg\max(u,v)$ iterations.
   todo!()
 }
 
@@ -152,9 +232,9 @@ mod tests {
       ((5046, 8004), 174),
       ((1 << 11, 1 << 13), 1 << 11),
     ];
-    let methods = [euclid, binary_brent];
+    const METHODS: [fn(u64, u64) -> u64; 3] = [euclid, binary_stein, binary_brent];
     for ((u, v), expected) in test_pairs {
-      for method in methods {
+      for method in METHODS {
         assert_eq!(method(u, v), expected);
         assert_eq!(method(v, u), expected);
       }
@@ -163,7 +243,7 @@ mod tests {
 
   #[test]
   fn check_binary() {
-    let gcd = binary_brent(40902, 24140);
+    let gcd = binary_stein(40902, 24140);
     println!("gcd={gcd}");
   }
 }
