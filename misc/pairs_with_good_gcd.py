c001aff0af44324f17bea692e7cc2815347cb958
Hugo Sanz González <80487270+hsanzg@users.noreply.github.com>
2025-12-26 22:30:18 +0100

    Fix off-by-one error in gcd pair generator script

--- misc/pairs_with_good_gcd.py
+++ misc/pairs_with_good_gcd.py
@@ -10,12 +10,12 @@ u_start, u_end, v_start, v_end = 7600, 7700, 3400, 3490
 u_start, u_end, v_start, v_end = 30000, 35000, 18000, 30000
 #u_start, u_end, v_start, v_end = 110, 130, 550, 555
 
-u_digits, v_digits = 4, 4
-u_start, v_start = 10 ** u_digits, 10 ** v_digits
+u_digits, v_digits = 1, 2
+u_start, v_start = 10 ** (u_digits - 1), 10 ** (v_digits - 1)
 u_end, v_end = u_start * 10, v_start * 10
 
-good_threshold = .001 # relative to `min(u,v)`.
-too_good_threshold = .08 # exclusive.
+good_threshold = .45 # relative to `min(u,v)`.
+too_good_threshold = .7 # exclusive.
 #good_threshold, too_good_threshold = .5, .99
 limit = 100 # how many to print.
 mode = 'random' # or 'exhaustive'.
